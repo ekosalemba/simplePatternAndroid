4e41a5758e48bb2b2d025ccdba4ee0e3e9bc12a3
Eko Aprianto <ekosalemba@gmail.com>
2016-09-08 12:06:30 +0700

    adding one more sample reactive

--- SimplePatternAndroid/app/src/main/java/com/simplepatternandroid/network/AppHeaderRequestInterceptor.java
+++ SimplePatternAndroid/app/src/main/java/com/simplepatternandroid/network/AppHeaderRequestInterceptor.java
@@ -11,6 +11,7 @@ import okhttp3.Response;
 
 public class AppHeaderRequestInterceptor implements Interceptor {
     public static final String X_APP_VERSION = "X-AppVersion";
+    public static final String X_AUTHORIZATION = "authorization";
 
     private final String appVersionName;
 
@@ -22,7 +23,9 @@ public class AppHeaderRequestInterceptor implements Interceptor {
     public Response intercept(Chain chain) throws IOException {
         okhttp3.Request request = chain.request();
         okhttp3.Request.Builder builder = request.newBuilder()
-                .addHeader(X_APP_VERSION, appVersionName);
+                .addHeader(X_APP_VERSION, appVersionName)
+                .addHeader(X_AUTHORIZATION, "Basic ZWtvc2FsZW1iYTpFa28xMjM=")
+                .addHeader("accept", "application/json");
         okhttp3.Request newRequest = builder.build();
         return chain.proceed(newRequest);
     }
